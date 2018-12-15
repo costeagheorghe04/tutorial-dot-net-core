85b87af458d38006314030add308ff3acac71379
costeagheorghe04 <costea.gheorghe.04@gmail.com>
2018-12-15 13:36:47 +0200

    format d0cument

--- src/ContosoUniversity/Data/DbInitializer.cs
+++ src/ContosoUniversity/Data/DbInitializer.cs
@@ -8,7 +8,8 @@ namespace ContosoUniversity.Data
 {
     public class DbInitializer
     {
-        public static void Initialize(SchoolContext context) {
+        public static void Initialize(SchoolContext context)
+        {
 
             if (context.Student.Any())
             {
@@ -17,14 +18,14 @@ namespace ContosoUniversity.Data
             }
             var student = new Student[]
             {
-            new Student{FirstMidName="gheorghe", LastName="ion", EnrollmentDate=DateTime.Parse("1999-11-12")},
-            new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
-            new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
-            new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+                new Student{FirstMidName="gheorghe", LastName="ion", EnrollmentDate=DateTime.Parse("1999-11-12")},
+                new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+                new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+                new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
             };
             foreach (Student s in student)
             {
@@ -33,13 +34,13 @@ namespace ContosoUniversity.Data
             context.SaveChanges();
             var courses = new Course[]
             {
-              new Course{CourseID=1050,Title="Chemistry",Credits=3},
-            new Course{CourseID=4022,Title="Microeconomics",Credits=3},
-            new Course{CourseID=4041,Title="Macroeconomics",Credits=3},
-            new Course{CourseID=1045,Title="Calculus",Credits=4},
-            new Course{CourseID=3141,Title="Trigonometry",Credits=4},
-            new Course{CourseID=2021,Title="Composition",Credits=3},
-            new Course{CourseID=2042,Title="Literature",Credits=4}
+                new Course{CourseID=1050,Title="Chemistry",Credits=3},
+                new Course{CourseID=4022,Title="Microeconomics",Credits=3},
+                new Course{CourseID=4041,Title="Macroeconomics",Credits=3},
+                new Course{CourseID=1045,Title="Calculus",Credits=4},
+                new Course{CourseID=3141,Title="Trigonometry",Credits=4},
+                new Course{CourseID=2021,Title="Composition",Credits=3},
+                new Course{CourseID=2042,Title="Literature",Credits=4}
             };
             foreach (Course c in courses)
             {
